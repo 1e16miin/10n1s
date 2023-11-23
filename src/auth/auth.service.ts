045b9fa702f2d9d780621adc9000ba267fdbe843
Gyeongmin Park <ddogi1513@naver.com>
2023-11-23 22:10:58 +0900

    add swagger docs

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -9,15 +9,7 @@ import { UserService } from "@user/user.service";
 @Injectable()
 export class AuthService {
   constructor(private readonly userService: UserService, private readonly jwtService: JwtService) {}
-
-  // setAccessToken({user, res}){
-    
-  // }
-
-  // setRefreshToken({user, res}){
-
-  // }
-  
+  //TODO: RefreshToken 추가
   async socialLogin(socialUser: any) {
     let user = await this.userService.findByEmail(socialUser.email);
 
